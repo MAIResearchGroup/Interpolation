5972db77d47f7eb597daa5995cef2ef10d7ad26c
LoOny <loonybiker@yandex.ru>
2016-03-23 21:23:59 +0300

    Fix #10
    Исправил ошибку определения интервала

--- MatLab2014b/LinearInterp.m
+++ MatLab2014b/LinearInterp.m
@@ -1,14 +1,12 @@
 function [ res ] = LinearInterp( arguments, functionValues, value )
     len = numel(arguments);
-    first = arguments(1);
-    last  = arguments(end);
 
-    if value <= first
+    if value <= arguments(1)
         res = functionValues(1);
         return
     end
     
-    if value >= last
+    if value >= arguments(len)
         res = functionValues(end);
         return
     end
@@ -17,7 +15,7 @@ function [ res ] = LinearInterp( arguments, functionValues, value )
     koeff(2) = 1;
 
     for i = 2:len
-        if value >= arguments(i-1) 
+        if value >= arguments(i-1) && value <= arguments(i)
             koeff(1) = (functionValues(i) - functionValues(i-1)) / (arguments(i) - arguments(i-1));
             koeff(2) = functionValues(i-1) - koeff(1)*arguments(i-1);
             break
